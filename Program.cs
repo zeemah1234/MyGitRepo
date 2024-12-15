ed3c6a115b3504a99054e68ec7de25ca08669e13
Azeemah <balogunazeemah0@gmail.com>
2024-12-15 07:25:39 +0100

    master

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class RandomStringGenerator {
+    public static void Main(String[] args) {
+        int length = 10; // Length of the string
+        char[] randomChars = new char[length]; // Array to store random characters
+        
+        Random random = new Random();
+        
+        for (int i = 0; i < length; i++) {
+            // Generating a random character between 'a' and 'z'
+            randomChars[i] = (char) (random.NextInt64(26) + 'a');
+        }
+        
+        // Converting the char array to a String
+        String randomString = new String(randomChars);
+        
+        // Printing the random string
+       Console.WriteLine("Enter a string: " + randomString);
+    }
+}
